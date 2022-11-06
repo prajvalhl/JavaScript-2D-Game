7090d05e299af7bae09f2db15f4213385db85ce9
Prajval <prajvalhl@gmail.com>
2022-11-06 14:34:13 +0530

    updated script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,61 @@ window.addEventListener("load", function () {
   const ctx = canvas.getContext("2d");
   canvas.width = 1500;
   canvas.height = 500;
+
+  class InputHandler {}
+
+  class Projectile {}
+
+  class Particle {}
+
+  class Player {
+    constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 190;
+      this.x = 20;
+      this.y = 100;
+      this.speedY = 0;
+    }
+
+    update() {
+      this.y += this.speedY;
+    }
+
+    draw(context) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+
+  class Enemy {}
+
+  class Layer {}
+
+  class Background {}
+
+  class UI {}
+
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+    }
+
+    update() {
+      this.player.update();
+    }
+
+    draw(context) {
+      this.player.draw(context);
+    }
+  }
+
+  const game = new Game(canvas.width, canvas.height);
+
+  function animate() {
+    game.update();
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
 });
